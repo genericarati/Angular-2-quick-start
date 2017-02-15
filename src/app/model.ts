b5356dea7491cd246b17c503085750e1bfcb1317
Arati <genericarati@gmail.com>
2017-02-14 22:13:47 -0500

    Angular 2 quickstart

---
+++ src/app/model.ts
@@ -0,0 +1,12 @@
+export class Customer  {
+  name : string ;
+  address : Address ;
+  id : string;
+}
+
+export class Address {
+  street : string ;
+  state : string ;
+  city : string ;
+  region : string ;
+}
